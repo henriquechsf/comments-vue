45ba211128b2e94e6303a8d6e4f97120e67eb302
Carlos Henrique <ch.ferreir@hotmail.com>
2020-03-01 13:24:06 -0300

    refatorando em single components

---
+++ vuecli-comments/src/App.vue
@@ -0,0 +1,15 @@
+<template></template>
+
+<script>
+import Comment from "./components/Comment";
+
+export default {
+  name: "app",
+  components: {
+    Comment
+  }
+};
+</script>
+
+<style>
+</style>
